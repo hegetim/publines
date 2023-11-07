01f31e561c0c0ea5e8aef7aa2e13a95d0f60df8f
Tim Hegemann <tim.hegemann@uni-wuerzburg.de>
2023-11-07 20:31:01 +0100

    sbcm stub

---
+++ src/model/Sbcm.ts
@@ -0,0 +1,24 @@
+import { Author, Publication } from "./Metadata";
+
+export interface Storyline {
+    authorIds: string[],
+    meetings: number[][],
+}
+
+export const mkStoryline = (publications: Publication[]): Storyline => {
+    let authors: Map<string, Author> = new Map();
+    for (const publ of publications) {
+        for (const contributor of publ.authors) {
+            if (!authors.has(contributor.id)) {
+                authors.set(contributor.id, contributor);
+            }
+        }
+    }
+    const orderedAuthors = [...authors.keys()];
+    let meetings: number[][] = [];
+    for (const publ of publications) {
+        const meeting = publ.authors.map(contributor => orderedAuthors.indexOf(contributor.id));
+        meetings.push(meeting.toSorted((a, b) => b - a));
+    }
+    return { authorIds: orderedAuthors, meetings };
+}
