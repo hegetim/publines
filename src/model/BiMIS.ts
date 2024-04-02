c8b1741c4df148c90b0903bdf7bd3b67a30c2fc2
Tim Hegemann <tim.hegemann@uni-wuerzburg.de>
2024-04-02 22:38:38 +0200

    bipartite mis implementation

---
+++ src/model/BiMIS.ts
@@ -0,0 +1,70 @@
+import _ from "lodash";
+import BitSet from "bitset";
+
+/// based on https://cp-algorithms.com/graph/kuhn_maximum_bipartite_matching.html
+const maxMatching = (g: (v: number) => number[], a: number, b: number) => {
+    const matching = Array.from({ length: b }, () => -1);
+    const used = new BitSet();
+
+    const step = (v: number) => {
+        if (used.get(v)) { return false; }
+        used.set(v, 1);
+        for (const to of g(v)) {
+            if (matching[to] === -1 || step(matching[to]!)) {
+                matching[to] = v;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    _.range(0, a).forEach(v => {
+        used.clear();
+        step(v);
+    })
+
+    return matching;
+}
+
+const mkMis = (g: (v: number) => number[], matching: number[], a: number): [BitSet, BitSet] => {
+    const isa = new BitSet();
+    isa.flip();
+    matching.forEach(v => {
+        if (v > -1) {
+            isa.set(v, 0)
+        }
+    });
+
+    const isb = new BitSet();
+    isb.flip();
+
+    const check = (v: number) => {
+        if (isa.get(v) === 1) {
+            g(v).forEach(w => {
+                if (isb.get(w) === 1) {
+                    isb.set(w, 0);
+                    if (matching[w] !== -1) {
+                        isa.set(matching[w]!, 1);
+                        check(matching[w]!);
+                    }
+                }
+            });
+        }
+    }
+
+    _.range(0, a).forEach(check);
+
+    return [isa, isb];
+}
+
+const assertIndependent = (g: (v: number) => number[], a: number, isa: BitSet, isb: BitSet) => {
+    _.range(0, a).forEach(v => {
+        if (isa.get(v) === 1) {
+            g(v).forEach(w => {
+                if (isb.get(w) === 1) {
+                    throw new Error(`${v} and ${w} are not independent`)
+                }
+            });
+        }
+    });
+}
